4c15518ff5185f5d63419e6cd4cb9ed4e2782ccc
battery-staple <56455596+battery-staple@users.noreply.github.com>
2023-03-25 09:40:45 -0400

    Added new SI prefixes (quetta/quecto, ronna/ronto), fixed #3

--- src/commonMain/kotlin/units/SIPrefixes.kt
+++ src/commonMain/kotlin/units/SIPrefixes.kt
@@ -1,27 +1,33 @@
 @file:Suppress("unused")
 package com.batterystaple.kmeasure.units
 
-inline val yotta: Double get() = 10E24
-inline val zetta: Double get() = 10E21
-inline val exa: Double get() = 10E18
-inline val peta: Double get() = 10E15
-inline val tera: Double get() = 10E12
-inline val giga: Double get() = 10E9
-inline val mega: Double get() = 10E6
-inline val kilo: Double get() = 10E3
-inline val hecto: Double get() = 10E2
-inline val deca: Double get() = 10E1
-inline val deci: Double get() = 10E-1
-inline val centi: Double get() = 10E-2
-inline val milli: Double get() = 10E-3
-inline val micro: Double get() = 10E-6
-inline val nano: Double get() = 10E-9
-inline val pico: Double get() = 10E-12
-inline val femto: Double get() = 10E-15
-inline val atto: Double get() = 10E-18
-inline val zepto: Double get() = 10E-21
-inline val yocto: Double get() = 10E-24
+inline val quetta: Double get() = 1E30
+inline val ronna: Double get() = 1E27
+inline val yotta: Double get() = 1E24
+inline val zetta: Double get() = 1E21
+inline val exa: Double get() = 1E18
+inline val peta: Double get() = 1E15
+inline val tera: Double get() = 1E12
+inline val giga: Double get() = 1E9
+inline val mega: Double get() = 1E6
+inline val kilo: Double get() = 1E3
+inline val hecto: Double get() = 1E2
+inline val deca: Double get() = 1E1
+inline val deci: Double get() = 1E-1
+inline val centi: Double get() = 1E-2
+inline val milli: Double get() = 1E-3
+inline val micro: Double get() = 1E-6
+inline val nano: Double get() = 1E-9
+inline val pico: Double get() = 1E-12
+inline val femto: Double get() = 1E-15
+inline val atto: Double get() = 1E-18
+inline val zepto: Double get() = 1E-21
+inline val yocto: Double get() = 1E-24
+inline val ronto: Double get() = 1E-27
+inline val quecto: Double get() = 1E-30
 
+inline val Int.quetta: Double get() = this * com.batterystaple.kmeasure.units.quetta
+inline val Int.ronna: Double get() = this * com.batterystaple.kmeasure.units.ronna
 inline val Int.yotta: Double get() = this * com.batterystaple.kmeasure.units.yotta
 inline val Int.zetta: Double get() = this * com.batterystaple.kmeasure.units.zetta
 inline val Int.exa: Double get() = this * com.batterystaple.kmeasure.units.exa
@@ -42,7 +48,11 @@ inline val Int.femto: Double get() = this * com.batterystaple.kmeasure.units.fem
 inline val Int.atto: Double get() = this * com.batterystaple.kmeasure.units.atto
 inline val Int.zepto: Double get() = this * com.batterystaple.kmeasure.units.zepto
 inline val Int.yocto: Double get() = this * com.batterystaple.kmeasure.units.yocto
+inline val Int.ronto: Double get() = this * com.batterystaple.kmeasure.units.ronto
+inline val Int.quecto: Double get() = this * com.batterystaple.kmeasure.units.quecto
 
+inline val Long.quetta: Double get() = this * com.batterystaple.kmeasure.units.quetta
+inline val Long.ronna: Double get() = this * com.batterystaple.kmeasure.units.ronna
 inline val Long.yotta: Double get() = this * com.batterystaple.kmeasure.units.yotta
 inline val Long.zetta: Double get() = this * com.batterystaple.kmeasure.units.zetta
 inline val Long.exa: Double get() = this * com.batterystaple.kmeasure.units.exa
@@ -63,7 +73,12 @@ inline val Long.femto: Double get() = this * com.batterystaple.kmeasure.units.fe
 inline val Long.atto: Double get() = this * com.batterystaple.kmeasure.units.atto
 inline val Long.zepto: Double get() = this * com.batterystaple.kmeasure.units.zepto
 inline val Long.yocto: Double get() = this * com.batterystaple.kmeasure.units.yocto
+inline val Long.ronto: Double get() = this * com.batterystaple.kmeasure.units.ronto
+inline val Long.quecto: Double get() = this * com.batterystaple.kmeasure.units.quecto
 
+
+inline val Double.quetta: Double get() = this * com.batterystaple.kmeasure.units.quetta
+inline val Double.ronna: Double get() = this * com.batterystaple.kmeasure.units.ronna
 inline val Double.yotta: Double get() = this * com.batterystaple.kmeasure.units.yotta
 inline val Double.zetta: Double get() = this * com.batterystaple.kmeasure.units.zetta
 inline val Double.exa: Double get() = this * com.batterystaple.kmeasure.units.exa
@@ -84,3 +99,5 @@ inline val Double.femto: Double get() = this * com.batterystaple.kmeasure.units.
 inline val Double.atto: Double get() = this * com.batterystaple.kmeasure.units.atto
 inline val Double.zepto: Double get() = this * com.batterystaple.kmeasure.units.zepto
 inline val Double.yocto: Double get() = this * com.batterystaple.kmeasure.units.yocto
+inline val Double.ronto: Double get() = this * com.batterystaple.kmeasure.units.ronto
+inline val Double.quecto: Double get() = this * com.batterystaple.kmeasure.units.quecto
